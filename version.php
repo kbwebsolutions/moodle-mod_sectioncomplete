8a93aeb214d58347b1f04b1b10c5b17c95355cfd
Kieran Briggs <kbriggs@chartered.college>
2020-11-03 10:58:13 +0000

    Version Bump

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020100900;       // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2020110300;       // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2020060900;    // Requires this Moodle version
 $plugin->component = 'mod_sectioncomplete'; // Full name of the plugin (used for diagnostics)
